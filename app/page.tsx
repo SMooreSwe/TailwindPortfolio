5b59f98b2cf57417272c8a444b445433ed4bd840
SMooreSwe <stevedm88@gmail.com>
2023-04-26 17:25:47 +0200

    added footer

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
+    <>
     <main className='flex ml-20 justify-between content-center h-screen'>
       <div className='flex justify-center flex-col py-[25%] pl-[15%] gap-5'>
       <h1 className='text-8xl'>Stephen Moore</h1>
@@ -15,5 +16,18 @@ export default function Home() {
       </div>
       <div className='fixed right-[-700px] w-[2000px] h-auto aspect-square shadow-md shadow-white rounded-full z-0'></div>
     </main>
+    <footer className='fixed  w-screen h-20 bottom-0 flex justify-center content-center gap-5'>
+      <div className=''>JS</div>
+      <div className=''>TS</div>
+      <div className=''>Node</div>
+      <div className=''>GIT</div>
+      <div className=''>Express</div>
+      <div className=''>React</div>
+      <div className=''>Next</div>
+      <div className=''>Firestore</div>
+      <div className=''>Mongo</div>
+      <div className=''>PostgresQL</div>
+    </footer>
+    </>
   )
 }
