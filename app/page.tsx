c08eaf89a4c53e252ecbd0e52259c4d9c2be8e9c
SMooreSwe <stevedm88@gmail.com>
2023-04-26 18:23:18 +0200

    multiple buttons and footer placeholder done

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,12 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import NavBar from './components/NavBar'
 import Link from 'next/link'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faRectangleList } from '@fortawesome/free-solid-svg-icons'
 
 const inter = Inter({ subsets: ['latin'] })
-
+//link to icons github <a target="_blank" href="https://icons8.com/icon/12599/github">GitHub</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
+//link to linkedIn <a target="_blank" href="https://icons8.com/icon/8808/linkedin">LinkedIn</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
 export default function Home() {
   return (
     <>
@@ -12,9 +15,19 @@ export default function Home() {
       <div className='flex justify-center flex-col py-[25%] pl-[15%] gap-5'>
       <h1 className='text-8xl'>Stephen Moore</h1>
       <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, architecto?</h3>
-      <Link href={'/projects'}><button className='main__button'>See my projects here</button></Link>
+      <div className='flex px-1 gap-2'>
+        <Link href={'/projects'}>
+          <button className='main__button flex gap-1'><FontAwesomeIcon icon={faRectangleList} className="w-4 h-4 mt-[0.38rem]" />See my Projects</button>
+        </Link>
+        <Link href={'https://github.com/SMooreSwe'} target="_blank">
+          <button className='main__button flex gap-1'> <img src="/icons8-github-50.png" alt="" className="w-4 h-4 mt-[0.38rem]"/> Check out my github</button>
+        </Link>
+        <Link href={'https://www.linkedin.com/in/stephen-moore-swe/'} target="_blank">
+          <button className='main__button flex gap-1'> <img src="/icons8-linkedin-50.png" alt="" className="w-4 h-4 mt-[0.38rem]"/> Connect on LinkedIn</button>
+        </Link>
       </div>
-      <div className='fixed right-[-700px] w-[2000px] h-auto aspect-square shadow-md shadow-white rounded-full z-0'></div>
+      </div>
+      <div className='fixed right-[-700px] w-[2000px] h-auto aspect-square shadow-md shadow-white rounded-full z-[-100]'></div>
     </main>
     <footer className='fixed  w-screen h-20 bottom-0 flex justify-center content-center gap-5'>
       <div className=''>JS</div>
@@ -23,9 +36,11 @@ export default function Home() {
       <div className=''>GIT</div>
       <div className=''>Express</div>
       <div className=''>React</div>
+      <div className=''>Redux/RTK</div>
       <div className=''>Next</div>
+      <div className=''>Tailwind</div>
       <div className=''>Firestore</div>
-      <div className=''>Mongo</div>
+      <div className=''>MongoDB</div>
       <div className=''>PostgresQL</div>
     </footer>
     </>
