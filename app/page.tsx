b479b73e08f73ae47d8c5bfccedfa576d63345fe
SMooreSwe <stevedm88@gmail.com>
2023-04-26 14:44:32 +0200

    button working and styling moved to globals

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import NavBar from './components/NavBar'
+import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -10,7 +11,7 @@ export default function Home() {
       <div className='flex justify-center flex-col py-[25%] pl-[15%] gap-5'>
       <h1 className='text-8xl'>Stephen Moore</h1>
       <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, architecto?</h3>
-      <button className='text-xl bg-gray-800 rounded-2xl py-2 px-5 w-fit  hover:bg-emerald-600 hover:rounded-xl transition-all duration-200 ease-linear'>See my projects here</button>
+      <Link href={'/projects'}><button className='main__button'>See my projects here</button></Link>
       </div>
       <div className='fixed right-[-700px] w-[2000px] h-auto aspect-square shadow-md shadow-white rounded-full z-0'></div>
     </main>
