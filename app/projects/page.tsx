97e8620e6a8453eb7897568947d12fdcfed7436e
SMooreSwe <stevedm88@gmail.com>
2023-05-05 12:37:05 +0200

    project responsiveness improved

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -9,7 +9,7 @@ const Projects = () => {
   return (
     <>
     <header className='max-sm:pt-[20vh] pt-[10vh] pb-[7vh] mx-12 text-6xl'>My Projects</header>
-      <div className='project-scroller mx-12'>
+      <div className='project-scroller mx-1 sm:mx-12'>
         {projectList.map((project, index) => {
           return (
           <TimelineSection key={uuidv4()} name={project}/>
