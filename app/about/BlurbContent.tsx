6750174345a5423d047562f4bf64d42250686700
SMooreSwe <stevedm88@gmail.com>
2023-06-09 09:30:32 +0200

    added to about me page

--- app/about/BlurbContent.tsx
+++ app/about/BlurbContent.tsx
@@ -4,7 +4,7 @@ import SocialButtons from '../components/SocialButtons'
 const BlurbContent = () => {
   return (
     <>
-        <p>I live in Stockholm, with my wife, Stina. I graduated from {'</SALT>'} as a Full-Stack JavaScript Web Developer. The intensive course involved 500 hours of mob programming, weekly coding tests, agile methodologies and weekly demos for 12 weeks.</p>
+        <p>I live in Stockholm, with my wife, Stina. I graduated from {'</SALT>'} as a Full-Stack JavaScript Web Developer, following over 500 hours of mob programming. I then completed an accelerated course in C# and .NET.</p>
         <div className='flex gap-2'>
           <img src="/wigPic.jpg" alt="my old work uniform" className='w-14 md:w-20 lg:w-24 xl:w-32 aspect-auto blob3 border-2 border-secondary'/>
           <p className='align-middle my-auto'>Prior to moving to tech, I worked as a Criminal Law and Family Law Barrister in the UK (yes, I really worked dressed like that!).</p>
