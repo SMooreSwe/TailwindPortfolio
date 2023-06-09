3c29572af4ee70bcc93a0e293b4a9202773b99e3
SMooreSwe <stevedm88@gmail.com>
2023-06-09 09:21:52 +0200

    added c# to footer

--- app/components/FooterContent.tsx
+++ app/components/FooterContent.tsx
@@ -7,11 +7,11 @@ const FooterContent = () => {
         <div className='icon-container'>
             <div className=''><img src="/JS-icon.png" alt="JavaScript Icon" className="h-8"/></div>
             <div className=''><img src="/typescript-icon.png" alt="TypeScript Icon" className="h-8"/></div>
+            <div className=''><img src="/c-sharp.png" alt="C-Sharp Icon" className="h-8"/></div>
             <div className=''><img src="/node-icon.png" alt="NodeJS Icon" className="h-8"/></div>
             <div className=''><img src="/github-icon.png" alt="Github Icon" className="h-8"/></div>
-            <div className=''><img src="/expressjs-icon.png" alt="ReactJS Icon" className="h-8"/></div>
-            <div className=''><img src="/react-icon.png" alt="ExpressJS Icon" className="h-8"/></div>
-            <div className=''><img src="/redux-icon.png" alt="Redux Icon" className="h-8"/></div>
+            <div className=''><img src="/expressjs-icon.png" alt="ExpressJS Icon" className="h-8"/></div>
+            <div className=''><img src="/react-icon.png" alt="React Icon" className="h-8"/></div>
             <div className=''><img src="/next.svg" alt="Next Icon" className="h-6 mt-1"/></div>
             <div className=''><img src="/tailwind-icon.png" alt="Tailwind CSS Icon" className="h-8"/></div>
             <div className=''><img src="/firebase-icon.png" alt="Firebase Icon" className="h-8"/></div>
