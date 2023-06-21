901b94d14f1f6bf741e725044ba13f7d8a30db1d
SMooreSwe <stevedm88@gmail.com>
2023-06-21 09:48:49 +0200

    rearranged carosel

--- app/projects/projectList.ts
+++ app/projects/projectList.ts
@@ -20,6 +20,23 @@ export const projectList : Project[] = [
     ]
   }, 
   {
+    heading: 'Puppy Picker',
+    blurb: 'This mobile-responsive, full stack App consists of a React UI built using NextJS, a RESTful C# API and a Azure SQL database. Pictures for each puppy are obtained from the Unsplash API and relate to the selected dog\'s breed. The UI allows users easy access to CRUD functionality.',
+    src: '/puppiesGIF.gif',
+    links: [
+      {
+        text: 'Visit the site',
+        href: 'https://puppypicker.vercel.app/',
+        icon: faGlobe,
+      },
+      {
+        text: 'See the Code',
+        href: 'https://github.com/SMooreSwe/puppies-frontend',
+        icon: faCode,
+      }
+    ]
+  },
+  {
     heading: 'Tech Interview Helper',
     blurb: 'This full stack project was built in a day, as part of a Hack-Day event. It uses a RESTful Express API, which communicates with a MongoDB database and a third party API "Techy". The project includes a working system for account creation, user login and saving data to user profiles.',
     src: '/techGif.gif',
@@ -60,23 +77,6 @@ export const projectList : Project[] = [
       }
     ]
   },
-  {
-    heading: 'Puppy Picker',
-    blurb: 'This mobile-responsive, full stack App consists of a React UI built using NextJS, a RESTful C# API and a Azure SQL database. Pictures for each puppy are obtained from the Unsplash API and relate to the selected dog\'s breed. The UI allows users easy access to CRUD functionality.',
-    src: '/puppiesGIF.gif',
-    links: [
-      {
-        text: 'Visit the site',
-        href: 'https://puppypicker.vercel.app/',
-        icon: faGlobe,
-      },
-      {
-        text: 'See the Code',
-        href: 'https://github.com/SMooreSwe/puppies-frontend',
-        icon: faCode,
-      }
-    ]
-  }
 ]
 
 export const listLength = projectList.length;
